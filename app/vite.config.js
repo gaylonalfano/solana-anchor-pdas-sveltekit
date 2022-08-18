f9e9a895d008c4521699e63192fcdecdd7d89a97
gaylonalfano <gaylon.alfano@gmail.com>
2022-08-19 07:55:23 +0800

    Just do a fresh install of sveltekit BEFORE doing the upgrade. Next, need to run a migration script for routes.

--- app/vite.config.js
+++ app/vite.config.js
@@ -7,7 +7,15 @@ const config = {
 		'process.env.BROWSER': true
 	},
 	optimizeDeps: {
-		include: ['@project-serum/anchor', '@solana/web3.js', 'buffer']
+		include: ['@project-serum/anchor', '@solana/web3.js', 'buffer'],
+		// NOTE esbuildOptions & build config referenced from Github issue:
+		// https://github.com/svelte-on-solana/wallet-adapter/issues/42
+		esbuildOptions: {
+			target: 'esnext'
+		}
+	},
+	build: {
+		target: 'esnext'
 	}
 };
 
